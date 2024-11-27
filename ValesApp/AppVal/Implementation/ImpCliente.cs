7e02a83ad6c13b289e7138c6d69328e521ef801e
Kevin Licon <licon_kevin@outlook.com>
2024-11-26 20:37:57 -0600

    Agregar archivos de proyecto.

---
+++ ValesApp/AppVal/Implementation/ImpCliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ValesApp.Domain.Entity;
+using Windows.Networking;
+
+namespace ValesApp.AppVal.Implementation
+{
+    public class ImpCliente: Cliente
+    {
+        public ImpCliente(int phoneNumber, string name, string lastName)
+        {
+            this.phoneNumber = phoneNumber;
+            this.name = name;
+            this.lastName = lastName;
+        }
+
+        public int getNumber()
+        {
+            return phoneNumber;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getLastName()
+        {
+            return lastName;
+        }
+    }
+}
---
+++ ValesApp/DB/DataAccess.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValesApp.AppVal.Implementation;
+using Windows.Storage;
+
+namespace ValesApp.DB
+{
+    public static class DataAccess
+    {
+        private static string dbName = "MyValeDb.db";
+
+        public async static void InitDb()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync(dbName, CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, dbName);
+
+            using (SqliteConnection db = new SqliteConnection($"Filename = {dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS" +
+                    "Clientes (phoneNumber INTEGER PRIMARY KEY, " +
+                    "name VARCHAR(20), lastName VARCHAR(20))";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteNonQuery();
+
+            }
+
+        }
+
+        public static void InsertClient(ImpCliente cliente)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, dbName);
+
+            using (SqliteConnection db = new SqliteConnection($"Filename = {dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+
+                insertCommand.Connection = db;
+
+                insertCommand.ExecuteReader();
+
+            }
+
+
+        }
+    }
+}
